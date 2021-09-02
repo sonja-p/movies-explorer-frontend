2e174dca225c93bff9007da7993c28b80f679498
Sofia Puolakainen <sonja.puolaka@yandex.ru>
2021-09-02 11:33:03 +0300

    Добавлена разметка и стили для раздела AboutMe

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
 import './AboutMe.css';
+import photoMe from '../../images/photo-me.jpg';
 
 function AboutMe() {
   return (
-    <>
-      <div className="about-me">
-        AboutMe
+    <div className="about-me">
+      <h2 className="about-me__title">Студентrf</h2>
+      <div className="about-me__container">
+        <div className="about-me__text-container">
+          <h3 className="about-me__name">Софья</h3>
+          <p className="about-me__profession">Фронтенд-разработчик, 31 год</p>
+          <p className="about-me__text">
+            Я живу в Санкт-Петербурге,
+            закончила факультет технологии и предпринимательства РГПУ им.Герцена.
+            Я люблю шить и современные технологии,
+            долгое время работала в портнихой,
+            сейчас моя работа связана со швейной и вышивальной техникой.
+            А ещё увлекаюсь бегом.
+            Недавно, благодаря учебе в Яндекс.Практикуме, начала кодить.
+            После окончания курса планирую работать в сфере IT.
+          </p>
+
+          <div className="about-me__social-media">
+            <a href="https://www.instagram.com/soninson/" className="about-me__link" target="_blank" rel="noreferrer">Instagram</a>
+            <a href="https://github.com/sonja-p" className="about-me__link" target="_blank" rel="noreferrer">Github</a>
+          </div>
+        </div>
+        <img className="about-me__photo" alt="me" src={photoMe} />
       </div>
-    </>
+    </div>
   );
 }
 
