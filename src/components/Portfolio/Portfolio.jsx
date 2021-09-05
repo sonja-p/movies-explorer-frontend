68275698f1b9352eba43c2deac7b56a3632b26df
Sofia Puolakainen <sonja.puolaka@yandex.ru>
2021-09-05 23:34:48 +0300

    Добавлена разметка и стили для компонента Portfolio

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -3,11 +3,29 @@ import './Portfolio.css';
 
 function Portfolio() {
   return (
-    <>
-      <div className="portfolio">
-        Portfolio
-      </div>
-    </>
+    <div className="portfolio">
+      <h2 className="portfolio__title">Портфолио</h2>
+      <ul className="portfolio__links">
+        <li className="portfolio__item">
+          <a href="https://puolukka.nomoredomains.rocks" className="portfolio__link" target="_blank" rel="noreferrer">
+            <p className="portfolio__link-title">Статичный сайт</p>
+            <div className="portfolio__link-arrow" />
+          </a>
+        </li>
+        <li className="portfolio__item">
+          <a href="https://puolukka.nomoredomains.rocks" className="portfolio__link" target="_blank" rel="noreferrer">
+            <p className="portfolio__link-title">Адаптивный сайт</p>
+            <div className="portfolio__link-arrow" />
+          </a>
+        </li>
+        <li className="portfolio__item">
+          <a href="https://puolukka.nomoredomains.rocks" className="portfolio__link" target="_blank" rel="noreferrer">
+            <p className="portfolio__link-title">Одностраничное приложение</p>
+            <div className="portfolio__link-arrow" />
+          </a>
+        </li>
+      </ul>
+    </div>
   );
 }
 
