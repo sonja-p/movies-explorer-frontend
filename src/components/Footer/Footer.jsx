fa15ff7a37b034872cec7bab9b4384f973414c17
Sofia Puolakainen <sonja.puolaka@yandex.ru>
2021-09-06 11:43:11 +0300

    Добавлена разметка и стили для компонента Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,11 +3,27 @@ import './Footer.css';
 
 function Footer() {
   return (
-    <>
-      <div className="footer">
-        Подвал
+    <div className="footer">
+      <p className="footer__about-project">Учебный проект Яндекс.Практикум х BeatFilm.</p>
+      <div className="footer__container">
+        <p className="footer__copyright">
+          &copy;
+          {' '}
+          {new Date().getFullYear()}
+        </p>
+        <ul className="footer__links">
+          <li className="footer__links-item">
+            <a href="https://practicum.yandex.ru" className="footer__link" target="_blank" rel="noreferrer">Яндекс.Практикум</a>
+          </li>
+          <li className="footer__links-item">
+            <a href="https://github.com/sonja-p" className="footer__link" target="_blank" rel="noreferrer">Github</a>
+          </li>
+          <li className="footer__links-item">
+            <a href="https://www.instagram.com/soninson" className="footer__link" target="_blank" rel="noreferrer">Instagram</a>
+          </li>
+        </ul>
       </div>
-    </>
+    </div>
   );
 }
 
